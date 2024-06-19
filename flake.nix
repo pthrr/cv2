b1034c7aa9613be196fb380b991782d54ab04df1
pthrr <pthrr@users.noreply.github.com>
2024-06-19 20:48:10 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "cpp-template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, }: utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.default = pkgs.mkShell.override { stdenv = pkgs.gcc13Stdenv; } {
+      packages = with pkgs; [
+        cmake
+        conan
+        ninja
+        mold
+        ccache
+        python3
+      ];
+
+      shellHook = ''
+        export IN_NIX_SHELL=1
+      '';
+    };
+  });
+}
